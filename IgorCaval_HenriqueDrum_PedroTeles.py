6bd61be78014a80e4f3d2daf0fbd183a13d666bb
Henrique Drumond Perez <h.drumondperez@gmail.com>
2021-10-27 09:56:01 -0300

    commit

---
+++ IgorCaval_HenriqueDrum_PedroTeles.py
@@ -0,0 +1,348 @@
+import os
+import platform
+import random
+import time
+from unicodedata import normalize
+
+def boneco(n):
+   if n == 0:  
+    print(
+      '''
+                                  ___________.._______
+                                 | .__________))______|
+                                 | | / /      
+                                 | |/ /       
+                                 | | /        
+                                 | |/         
+                                 | |          
+                                 | |          
+                                 | |         
+                                 | |        
+                                 | |      
+                                 | |      
+                                 | |     
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |     
+                                 | |    
+                                 """""""""""|""""""""""|"|
+                                 |"|""""""""|""""""""'"|"|
+                                 | |                   | |
+                                 : :                   : : 
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      ''')
+
+   if n == 1:
+    print(
+      '''
+                                  ___________.._______
+                                 | .__________))______|
+                                 | | / /      ||
+                                 | |/ /       ||
+                                 | | /        ||.-''.
+                                 | |/         |/  _  \\
+                                 | |          ||  `/,/
+                                 | |          (\\`_.'
+                                 | |         
+                                 | |        
+                                 | |      
+                                 | |      
+                                 | |     
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |     
+                                 | |    
+                                 """""""""""|""""""""""|"|
+                                 |"|""""""""|""""""""'"|"|
+                                 | |                   | |
+                                 : :                   : : 
+                                 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      ''')
+   
+   if n == 2:
+    print(   
+      '''
+                                  ___________.._______
+                                 | .__________))______|
+                                 | | / /      ||
+                                 | |/ /       ||
+                                 | | /        ||.-''.
+                                 | |/         |/  _  \\
+                                 | |          ||  `/,/
+                                 | |          (\\`_.'
+                                 | |         .-`--'.
+                                 | |         Y . . Y
+                                 | |          |   | 
+                                 | |          | . |  
+                                 | |          |   |   
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |     
+                                 | |    
+                                 """""""""""|""""""""""|"|
+                                 |"|""""""""|""""""""'"|"|
+                                 | |                   | |
+                                 : :                   : : 
+                                 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      ''')
+
+   if n == 3:
+    print(
+      '''
+                                  ___________.._______
+                                 | .__________))______|
+                                 | | / /      ||
+                                 | |/ /       ||
+                                 | | /        ||.-''.
+                                 | |/         |/  _  \\
+                                 | |          ||  `/,/
+                                 | |          (\\`_.'
+                                 | |         .-`--'.
+                                 | |        /Y . . Y\\
+                                 | |       // |   | \\\\
+                                 | |      //  | . |  \\\\
+                                 | |     ')   |   |   (`
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |          
+                                 | |     
+                                 | |    
+                                 """""""""""|""""""""""|"|
+                                 |"|""""""""|""""""""'"|"|
+                                 | |                   | |
+                                 : :                   : : 
+                                 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      ''')
+
+   if n == 4:
+    print(
+      '''
+                                  ___________.._______
+                                 | .__________))______|
+                                 | | / /      ||
+                                 | |/ /       ||
+                                 | | /        ||.-''.
+                                 | |/         |/  _  \\
+                                 | |          ||  `/,/
+                                 | |          (\\`_.'
+                                 | |         .-`--'.
+                                 | |        /Y . . Y\\
+                                 | |       // |   | \\\\
+                                 | |      //  | . |  \\\\
+                                 | |     ')   |   |   (`
+                                 | |          || ||
+                                 | |          || ||
+                                 | |          || ||
+                                 | |          || ||
+                                 | |         / | | \\
+                                 | |         `-' `-' 
+                                 """""""""""|""""""""""|"|
+                                 |"|""""""""|""""""""'"|"|
+                                 | |                   | |
+                                 : :                   : : 
+                                 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      ''')
+
+   if n == 5:
+    print(
+      '''
+                                  ___________.._______
+                                 | .__________))______|
+                                 | | / /      ||
+                                 | |/ /       ||
+                                 | | /        ||
+                                 | |/         ||.-''.  
+                                 | |          |/  _  \\
+                                 | |          ||  `/,/
+                                 | |          (\\`_.'
+                                 | |         .-`--'.
+                                 | |        /Y . . Y\\
+                                 | |       // |   | \\\\
+                                 | |      //  | . |  \\\\
+                                 | |     ')   |   |   (`
+                                 | |          || ||
+                                 | |          || ||
+                                 | |          || ||
+                                 | |          || ||
+                                 | |         / | | \\
+                                 """"""""""|_`-' `-' |"""|
+                                 |"|"""""""\ \       '"|"|
+                                 | |        \ \        | |
+                                 : :         \ \       : : 
+                                 
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      ''')
+
+    
+        
+def vitoria():
+   print("""
+                                 //\                                  
+                                |/ \|                                 
+                __   _____   ___ ___  __   _____ _ __   ___ ___ _   _ 
+                \ \ / / _ \ / __/ _ \ \ \ / / _ \ '_ \ / __/ _ \ | | |
+                 \ V / (_) | (_|  __/  \ V /  __/ | | | (_|  __/ |_| |
+                  \_/ \___/ \___\___|   \_/ \___|_| |_|\___\___|\__,_|
+                                                       
+                                                       
+""")
+def derrota():
+   print("""
+                                  //\                      _            
+                                 |/ \|                    | |           
+                 __   _____   __  ___   _ __   ___ _ __ __| | ___ _   _ 
+                 \ \ / / _ \ / __/ _ \ | '_ \ / _ \ '__/ _` |/ _ \ | | |
+                  \ V / (_) | (_|  __/ | |_) |  __/ | | (_| |  __/ |_| |
+                   \_/ \___/ \___\___| | .__/ \___|_|  \__,_|\___|\__,_|
+                                       | |                              
+                                       |_|                              
+""")
+
+def titulo():
+   print("""
+ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+       _  ____   _____  ____      _____              ______ ____  _____   _____          
+      | |/ __ \ / ____|/ __ \    |  __ \   /\       |  ____/ __ \|  __ \ / ____|   /\    
+      | | |  | | |  __| |  | |   | |  | | /  \      | |__ | |  | | |__) | |       /  \   
+  _   | | |  | | | |_ | |  | |   | |  | |/ /\ \     |  __|| |  | |  _  /| |      / /\ \  
+ | |__| | |__| | |__| | |__| |   | |__| / ____ \    | |   | |__| | | \ \| |____ / ____ \ 
+  \____/ \____/ \_____|\____/    |_____/_/    \_\   |_|    \____/|_|  \_\\\_____/_/    \_\\
+
+ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+                                                                                     
+""")
+
+def limpaTela():
+   if platform.system() == 'Linux' or platform.system() == 'Darwin':
+      os.system('clear') 
+   else:
+      os.system('cls') 
+
+def regrasJogo():
+    print("As regras são simples:")
+    print("""  Você deve acertar a palavra secreta tentando adivinhar quais letras fazem
+parte de sua composição""")
+    print("  Para isso, você possui 5 tentativas.", end='\n\n\n')
+    print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=', end='\n\n')
+
+
+def dificuldade():
+   dificuldade = input("Em qual nível de dificuldade você gostaria de jogar? \n\n1 - Fácil\n2 - Difícil\n3 - Aleatório\n\nEscolha: ")
+   while dificuldade not in ['1', '2', '3'] or dificuldade == '':
+      limpaTela()
+      
+      titulo()
+      boneco(0)
+      
+      dificuldade = input("Selecione uma dificuldade válida! \n\n1 - Fácil\n2 - Difícil\n3 - Aleatório\n\nEscolha: ")
+
+   return dificuldade
+
+def escolhePalavra(dificuldade):
+   if dificuldade == "1":
+      arquivo = 'palavras_faceis.txt'
+   elif dificuldade == "2":
+      arquivo = 'palavras_dificeis.txt'
+   elif dificuldade == "3":
+      arquivo = 'palavras_aleatorias.txt'
+   
+   with open(arquivo, "r", encoding="utf-8") as arq:
+      listaPalavras = arq.read().split('\n')
+        
+   return listaPalavras[random.randint(0, len(listaPalavras) - 1)]
+
+def jogoDaForca():
+   limpaTela()
+
+   titulo()
+   boneco(0)
+
+   palavraCorreta = escolhePalavra(dificuldade()).upper()
+   palavraSemAcento = normalize('NFKD', palavraCorreta).encode('ASCII', 'ignore').decode('ASCII').upper()
+
+   #Apenas pra mostrar a palavra
+   print(palavraCorreta)
+   time.sleep(3)
+
+   acertosUsuario = ['_'] * len(palavraCorreta)
+   tentativasUsuario = []
+   
+   erros = 0
+   while erros <= 4 and ''.join(acertosUsuario) != palavraCorreta:
+      limpaTela()
+
+      titulo()
+      boneco(erros)
+      
+      print(' '.join(acertosUsuario), end='\n\n')
+
+      if tentativasUsuario:
+         print(f'Você já digitou a(s) letra(s) {", ".join(tentativasUsuario)}', end='\n\n')
+
+
+      letraUsuario = input('Digite uma letra: ').upper().strip()
+      letraUsuarioSemAcento = normalize('NFKD', letraUsuario).encode('ASCII', 'ignore').decode('ASCII')
+
+      # Validação da entrada
+      if len(letraUsuarioSemAcento) > 1 or letraUsuarioSemAcento == '':
+         print('Digite apenas uma letra!') if len(letraUsuarioSemAcento) > 1 else print('Digite uma letra!')
+         time.sleep(2)
+         continue
+      elif not letraUsuarioSemAcento.isalpha():
+         print('Digite uma letra do alfabeto!')
+         time.sleep(2)
+         continue
+      elif letraUsuarioSemAcento in tentativasUsuario:
+         print('Você já digitou essa letra, tente outra!')
+         time.sleep(2)
+         continue
+      
+      tentativasUsuario.append(letraUsuarioSemAcento)
+
+      # Troca o underline pela letra correta ou conta os erros
+      if letraUsuarioSemAcento in palavraSemAcento: 
+         for i in range(len(palavraSemAcento)):
+            if letraUsuarioSemAcento == palavraSemAcento[i]:
+               acertosUsuario[i] = palavraCorreta[i]
+      else:   
+         erros += 1
+
+
+   limpaTela()
+
+   titulo()
+   
+   if erros >= 5:
+      boneco(erros)
+      derrota()
+   else:
+      vitoria()
+
+   print(f"A palavra correta era {palavraCorreta}", end='\n\n')
+
+def iniciaJogo():
+   while True:
+      titulo()
+      regrasJogo()
+
+      comecaJogo = input('Aperte Enter para iniciar o jogo!'.center(89))
+      if comecaJogo == '':
+         break
+
+      limpaTela()
+   jogoDaForca()
+
+iniciaJogo()
